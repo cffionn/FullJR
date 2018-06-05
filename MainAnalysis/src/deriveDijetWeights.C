edd2d99d7e0ee9750ff963cf62311f0374c94d02
Chris <cfmcginn@mit.edu>
2018-06-05 12:37:52 +0200

    Added script for deriving dijet weights in pythia6

---
+++ MainAnalysis/src/deriveDijetWeights.C
@@ -0,0 +1,212 @@
+#include "TFile.h"
+#include "TTree.h"
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "Utility/include/checkMakeDir.h"
+#include "Utility/include/doGlobalDebug.h"
+#include "Utility/include/mntToXRootdFileString.h"
+#include "Utility/include/returnRootFileContentsList.h"
+
+//5.02 TeV
+const Int_t nPtHat = 12;
+const Int_t ptHat[nPtHat+1] = {15, 30, 50, 80, 100, 120, 170, 220, 280, 370, 460, 540, 10000};
+//const Double_t ptHatCrossSections[nPtHat+1] = {.5269, .03455, .004068, .0004959, .00007096, .00001223, .000003031, .0000007746, .0000001410, .00000003216, .00000001001, 0.00000};
+const Double_t ptHatCrossSections[nPtHat+1] = {.5335, .03378, .003778, .0004412, .0001511, .00006147, .00001018, .000002477, .0000006160, .0000001088, .00000002527,  0.000000007865, 0.00000000};
+
+//2.76TeV
+//const Int_t nPtHat = 9;
+//const Int_t ptHat[nPtHat+1] = {15, 30, 50, 80, 120, 170, 220, 280, 370, 10000};
+//const Double_t ptHatCrossSections[nPtHat+1] = {.5269, .03455, .004068, .0004959, .00007096, .00001223, .000003031, .0000007746, .0000001410, .00000003216, .00000001001, 0.00000};
+//const Double_t ptHatCrossSections[nPtHat+1] = {.2034, .01075, .001025, .00009865, .00001129, .000001465, .0000002837, .00000005323, .000000005934, 00000000.000000};
+
+
+int deriveDijetWeights(const std::string inConfigFileName)
+{
+  if(!checkFile(inConfigFileName) || inConfigFileName.find(".txt") == std::string::npos){
+    std::cout << "Given inConfigFileName \'" << inConfigFileName << "\' is invalid. return 1" << std::endl;
+    return 1;
+  }
+
+  std::vector<Int_t> pthats;
+  std::vector<std::string> fileList;
+
+  std::ifstream file(inConfigFileName.c_str());
+  std::string tempStr;
+  while(std::getline(file, tempStr)){    
+    while(tempStr.find(" ") != std::string::npos){tempStr.replace(tempStr.find(" "), 1, "");}
+    if(tempStr.size() == 0) continue;
+    if(tempStr.find(".root") != std::string::npos) fileList.push_back(tempStr);
+    else{
+      if(tempStr.substr(0, std::string("PTHAT=").size()).find("PTHAT=") != std::string::npos){
+	tempStr.replace(0, std::string("PTHAT=").size(), "");
+	while(tempStr.find(",") != std::string::npos){
+	  pthats.push_back(std::stod(tempStr.substr(0, tempStr.find(","))));
+	  tempStr.replace(0, tempStr.find(",")+1, "");
+	}
+	if(tempStr.size() != 0) pthats.push_back(std::stod(tempStr));
+      }
+      else std::cout << "WARNING: Line in \'" << inConfigFileName << "\', \'" << tempStr << "\' is invalid. check input" << std::endl;
+    }
+  }
+
+  file.close();
+
+  unsigned int pos = 0;
+  while(pthats.size() > pos){
+    bool didSwap = false;
+
+    for(unsigned int pI = pos+1; pI < pthats.size(); ++pI){
+      if(pthats.at(pI) < pthats.at(pos)){
+	int temp = pthats.at(pI);
+	pthats.at(pI) = pthats.at(pos);
+	pthats.at(pos) = temp;       
+	didSwap = true;
+      }
+    }
+
+    if(!didSwap) ++pos;
+  }
+
+  std::vector<Double_t> crossSections;
+  std::vector<Int_t> pthatCounts;
+
+  for(unsigned int pI = 0; pI < pthats.size(); pI++){
+    Int_t pthatPos = -1;
+    Int_t currentPthat = pthats.at(pI);
+
+    for(Int_t pI2 = 0; pI2 < nPtHat; ++pI2){
+      if(pthats.at(pI) == ptHat[pI2]){
+	crossSections.push_back(ptHatCrossSections[pI2]);
+	pthatCounts.push_back(0);
+	pthatPos = pI2;
+	break;
+      }
+    }
+
+    if(pthatPos < 0){
+      std::cout << "WARNING: Cannot find pthat \'" << currentPthat << "\' in array. please check for valid pthat. return 1" << std::endl;
+      return 1;     
+    }
+  }
+
+  pthats.push_back(ptHat[nPtHat]);
+  pthatCounts.push_back(0);
+  crossSections.push_back(ptHatCrossSections[nPtHat]);
+
+  if(doGlobalDebug) std::cout << __FILE__ << ", " << __LINE__ << std::endl; 
+
+  std::cout << "Processing \'" << fileList.size() << "\' files..." << std::endl;
+  for(unsigned int fileIter = 0; fileIter < fileList.size(); fileIter++){
+    std::string tempFileName = mntToXRootdFileString(fileList.at(fileIter));
+    std::cout << " Processing file " << fileIter << "/" << fileList.size() << ": \'" << fileList.at(fileIter) << "\'" << std::endl;
+    
+    TFile* inFile_p = NULL;
+    inFile_p = TFile::Open(tempFileName.c_str(), "READ");
+    if(inFile_p == 0){
+      std::cout << "given file \'" << tempFileName << "\' was not able to be read, continue" << std::endl;
+      continue;
+    }
+    else if(inFile_p->IsZombie()){
+      std::cout << "given file \'" << tempFileName << "\' was not able to be read, continue" << std::endl;
+      continue;
+    }
+    
+    std::vector<std::string> jetTreeList = returnRootFileContentsList(inFile_p, "TTree", "JetAnalyzer");
+    
+    if(jetTreeList.size() == 0){
+      std::cout << "given file \'" << tempFileName << "\' contains no valid jet trees, continue" << std::endl;
+      inFile_p->Close();
+      delete inFile_p;
+      continue;
+    }
+    
+    TTree* jetTree_p = (TTree*)inFile_p->Get(jetTreeList.at(0).c_str());
+    
+    if(doGlobalDebug) std::cout << __FILE__ << ", " << __LINE__ << std::endl; 
+    
+    Float_t ptHat_;
+    
+    jetTree_p->SetBranchStatus("*", 0);
+    jetTree_p->SetBranchStatus("pthat", 1);
+    
+    jetTree_p->SetBranchAddress("pthat", &ptHat_);
+    
+    const Int_t nEntries = jetTree_p->GetEntries();
+    const Int_t printInterval = TMath::Max(1, nEntries/20);
+
+    for(Int_t entry = 0; entry < nEntries; ++entry){
+      if(nEntries >= 50000 && entry%printInterval == 0) std::cout << "  Entry: " << entry << "/" << nEntries << std::endl;
+      jetTree_p->GetEntry(entry);
+            
+      for(Int_t ptHatIter = 0; ptHatIter < (Int_t)pthats.size()-1; ptHatIter++){
+	if(ptHat_ < pthats.at(ptHatIter+1)){
+	  pthatCounts.at(ptHatIter)++;
+	  break;
+	}
+      }
+    } 
+    
+    if(doGlobalDebug) std::cout << __FILE__ << ", " << __LINE__ << std::endl; 
+    
+    inFile_p->Close();
+    delete inFile_p;
+  }
+
+  if(doGlobalDebug) std::cout << __FILE__ << ", " << __LINE__ << std::endl;
+
+  ULong64_t multAdjust = 1;
+
+  std::vector<Double_t> weights;
+
+  bool isFirst = true;
+  for(unsigned int iter = 0; iter < pthats.size()-1; iter++){
+
+    std::cout << "Pthat: " << pthats.at(iter) << "-" << pthats.at(iter+1) << std::endl;
+    std::cout << " Entries: " << pthatCounts.at(iter) << std::endl;
+    Double_t weight = (crossSections.at(iter) - crossSections.at(iter+1))/(Double_t)pthatCounts.at(iter);
+    std::cout << " Weight: " << weight << std::endl;
+
+    while(isFirst && weight*multAdjust < 1.){
+      multAdjust *= 10;
+    }
+    isFirst = false;
+
+    std::cout << " Adjusted weight: " << weight*multAdjust << std::endl;
+
+    weights.push_back(weight*multAdjust);
+  }
+
+  std::cout << std::endl;
+  std::cout << "Weights for copy-paste: ";
+
+  for(unsigned int iter = 0; iter < pthats.size()-1; iter++){
+    std::cout << weights.at(iter) << ", ";
+  }
+  std::cout << std::endl;
+
+  std::cout << "Renorm weights for copy-paste: ";
+
+  for(unsigned int iter = 0; iter < pthats.size()-1; iter++){
+    std::cout << weights.at(iter)/weights.at(0) << ", ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+  if(argc != 2){
+    std::cout << "Usage: deriveDijetWeights.exe <inConfigFile>" << std::endl;
+    std::cout << "Number of args given: " << argc << std::endl;
+    for(int iter = 0; iter < argc; iter++){
+      std::cout << "  argv[" << iter << "]: " << argv[iter] << std::endl;
+    }
+    return -1;
+  }
+
+  return deriveDijetWeights(argv[1]);
+}
