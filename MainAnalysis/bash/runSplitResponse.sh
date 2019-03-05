583458014f15cd0bea5c91743782914dae00ec8b
Chris McGinn <cffionn@gmail.com>
2019-03-05 12:38:58 -0500

    Now w/ memory issues fixed, can run many combines at once quickly, fix runSplitResponse.sh

--- MainAnalysis/bash/runSplitResponse.sh
+++ MainAnalysis/bash/runSplitResponse.sh
@@ -80,29 +80,85 @@ wait
 DATEEND=`date +%Y%m%d`
 
 rVals=(ak3 ak4 ak6 ak8 ak10 akCs3 akCs4 akCs6 akCs8 akCs10)
+rPaths=()
+
+for r in "${rVals[@]}"
+do
+    rPath="NA"
+    for i in "${filesToProcess[@]}"
+    do
+	val=$(grep $r $i)
+	if [[ $val == "/"* ]]
+	then
+	    echo $val, $r
+	    rPath=${val%/*}
+	    break
+	fi
+    done
+
+    rPaths+=($rPath)
+done
+
+pos=0
+for r in "${rVals[@]}"
+do
+    echo $r, ${rPaths[$pos]}
+    pos=$((pos + 1))
+done
+
+
+pos=0
 for r in "${rVals[@]}"
 do
     files=""
 
-    count=$(ls output/$DATE/*$r*SPLIT*.root | wc -l)
-    if [[ -d output/$DATEEND ]]
+    countLocalDate=0
+    if [[ -d output/$DATE ]]
     then
-	counts2=$(ls output/$DATEEND/*$r*SPLIT*.root | wc -l)
-	counts=$((counts + $counts2))
+	countLocalDate=$(ls output/$DATE/*$r*SPLIT*.root | wc -l)
     fi
 
+    countLocalDateEnd=0
+    if [[ $DATE -ne $DATEEND ]]
+    then
+	if [[ -d output/$DATEEND ]]
+	then
+	    countLocalDateEnd=$(ls output/$DATEEND/*$r*SPLIT*.root | wc -l)
+	fi
+    fi
+    
+    countGlobalDate=0
+    if [[ -d ${rPaths[$pos]}/output/$DATE ]]
+    then
+	countGlobalDate=$(ls ${rPaths[$pos]}/output/$DATE/*$r*SPLIT*.root | wc -l)
+    fi
+
+    countGlobalDateEnd=0
+    if [[ $DATE -ne $DATEEND ]]
+    then
+	if [[ -d ${rPaths[$pos]}/output/$DATEEND ]]
+	then
+	    countGlobalDateEnd=$(ls ${rPaths[$pos]}/output/$DATEEND/*$r*SPLIT*.root | wc -l)
+	fi
+    fi
+    count=$((countLocalDate + $countLocalDateEnd + $countGlobalDate + $countGlobalDateEnd))
+    
 
     if [[ $count -eq 0 ]]
     then
+	pos=$((pos + 1))
 	continue
     fi
+
+    if [[ $countLocalDate -ne 0 ]]
+    then
+	for i in output/$DATE/*$r*SPLIT*.root
+	do
+	    files=$files$i,
+	done
+    fi
     
-    for i in output/$DATE/*$r*SPLIT*.root
-    do
-	files=$files$i,
-    done
-    
-    if [[ -d output/$DATEEND ]]
+    if [[ $countLocalDateEnd -ne 0 ]]
     then
 	for i in output/$DATEEND/*$r*SPLIT*.root
 	do
@@ -110,13 +166,31 @@ do
 	done	
     fi
 
+    if [[ $countGlobalDate -ne 0 ]]
+    then
+	for i in ${rPaths[$pos]}/output/$DATE/*$r*SPLIT*.root
+	do
+	    files=$files$i,
+	done
+    fi
+    
+    if [[ $countGlobalDateEnd -ne 0 ]]
+    then
+	for i in ${rPaths[$pos]}/output/$DATEEND/*$r*SPLIT*.root
+	do
+	    files=$files$i,
+	done	
+    fi
+
     ./bin/combineResponse.exe $files $r >& logs/$DATE/combineResponse_$r.log &
     jobCounts=$(ps | grep combineR | wc -l)
-    if [[ $jobCounts -ge 5 ]]
+    if [[ $jobCounts -ge $jobNumber ]]
     then
 	sleep 10
 	jobCounts=$(ps | grep combineR | wc -l)
     fi
+
+    pos=$((pos + 1))
 done
 
 wait
